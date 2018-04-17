6b88e3456b8b7a1aa97be005b97c772d80c6a9bb
iuriramos <iuri.srb@gmail.com>
2018-04-17 18:12:48 -0300

    Atualização Aula 17/04/18

---
+++ 17-abril/codigos_executados.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+max(1, 2, 3)
+def soma(a, b):
+    s = a + b
+    return s
+soma(1, 2) 
+c = soma(1, 2)
+c 
+def soma(a, b):
+    '''
+    Função que soma dois numeros e retorna o valor.
+    '''
+    s = a + b
+    return s
+help(soma) 
+import math
+math.sqrt
+help(math.sqrt)
+def area_retangulo(largura, altura):
+    '''
+    Função que calcula area de um retangulo
+    '''
+    area = largura * altura
+    return area
+area_retangulo(2, 5)
+def area_quadrado(lado):
+    return area_retangulo(lado, lado) 
+area_quadrado(5) 
+def area_triangulo(base, altura):
+    return area_retangulo(base, altura) / 2 
+ 
+area_triangulo(2, 10) 
+abs(-10)
+val_absoluto = abs(-10)
+val_absoluto
+val_absoluto + 20
+abs(-10) + 20
+def soma(a, b):
+    '''
+    Função que soma dois numeros e retorna o valor.
+    '''
+    s = a + b
+    print(s)
+    return s
+soma(1, 2) 
+def soma(a, b):
+    '''
+    Função que soma dois numeros e retorna o valor.
+    '''
+    s = a + b
+    print(s)
+    return s
+soma(1, 2)
+def soma(a, b):
+    '''
+    Função que soma dois numeros e retorna o valor.
+    '''
+    return a+b
---
+++ 17-abril/exemplo.py
@@ -0,0 +1,14 @@
+# c = 0
+
+def foo():
+    # a = c + 1
+    # print(c)
+    a = 1
+    print(a)
+    return a
+    
+# print(c)
+a = foo()
+# print(c)
+print(a)
+
